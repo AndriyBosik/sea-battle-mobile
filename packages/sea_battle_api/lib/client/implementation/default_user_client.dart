683f560bb5c6bceba92589294299ed0701cdd90e
AndriiBosyk <andriybosik2002@ukr.net>
2022-03-15 12:47:33 +0200

    Extract api pathes to consts

--- packages/sea_battle_api/lib/client/implementation/default_user_client.dart
+++ packages/sea_battle_api/lib/client/implementation/default_user_client.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:format/format.dart';
 import 'package:http/http.dart' as http;
 import 'package:sea_battle_api/client/abstraction/user_client.dart';
+import 'package:sea_battle_api/const/api.dart';
 import 'package:sea_battle_api/exception/user_request_failure.dart';
 import 'package:sea_battle_api/json/abstraction/json_service.dart';
 import 'package:sea_battle_entity/module.dart';
@@ -22,7 +24,8 @@ class DefaultUserClient implements UserClient {
 
   @override
   Future<UserEntity?> getUserByNickname(String nickname) async {
-    final Uri url = Uri.parse("$_apiUrl/api/users/$nickname");
+    final String urlPath = format(Api.getUser, {"nickname": nickname});
+    final Uri url = Uri.parse(_apiUrl + urlPath);
 
     final response = await _httpClient.get(url);
     if (response .statusCode != 200) {
@@ -35,7 +38,7 @@ class DefaultUserClient implements UserClient {
 
   @override
   Future<void> createUser({required UserEntity user}) async {
-    final Uri url = Uri.parse("$_apiUrl/api/users");
+    final Uri url = Uri.parse(_apiUrl + Api.createUser);
     final response = await _httpClient.post(
       url,
       headers: <String, String>{
---
+++ packages/sea_battle_api/lib/const/api.dart
@@ -0,0 +1,6 @@
+class Api {
+  static const _apiPrefix = "/api";
+
+  static const getUser = _apiPrefix + "/users/{nickname}";
+  static const createUser = _apiPrefix + "/users";
+}
